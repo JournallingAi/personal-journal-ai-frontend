9b88d51a742f266750bddbc3d73db9a64235d255
sanskriti <sanskriti@adobe.com>
2025-09-21 12:44:54 +0530

    Disable unused vars ESLint rules completely

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['react-app'],
   rules: {
-    'no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
